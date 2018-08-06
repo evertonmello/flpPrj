cc72410e7172f0d88b3fdb8ff815c33b1caf18c3
Everton <everton.mello@airstrip.com.br>
2018-08-06 19:17:47 -0300

    as

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
   title = 'app';
+  public val = 0
+  public items;  
+  public check = false;
+  public units = [
+    {label:'Litro', value: 0},
+    {label:'Quilograma', value: 1},
+    {label:'Unidade', value: 2},
+  ]
+  public cars = [
+    { name: '', unit: 1231,  amount: 1233, price:'dadas', perishable:false, valDate: new Date().toLocaleDateString(), fabDate: new Date().toLocaleDateString()},
+    { name: 'dsa', unit: 1231,  amount: 1233, price:'dadas', perishable:false, valDate: new Date().toLocaleDateString(), fabDate: new Date().toLocaleDateString()},
+    { name: 'dsa', unit: 1231,  amount: 1233, price:'dadas', perishable:false, valDate: new Date().toLocaleDateString(), fabDate: new Date().toLocaleDateString()},
+    { name: 'dsa', unit: 1231,  amount: 1233, price:'dadas', perishable:false, valDate: new Date().toLocaleDateString(), fabDate: new Date().toLocaleDateString()},
+    { name: 'dsa', unit: 1231,  amount: 1233, price:'dadas', perishable:false, valDate: new Date().toLocaleDateString(), fabDate: new Date().toLocaleDateString()},
+    { name: 'dsa', unit: 1231,  amount: 1233, price:'dadas', perishable:false, valDate: new Date().toLocaleDateString(), fabDate: new Date().toLocaleDateString()},
+    { name: 'dsa', unit: 1231,  amount: 1233, price:'dadas', perishable:false, valDate: new Date().toLocaleDateString(), fabDate: new Date().toLocaleDateString()},
+  ];
+  
+
+
+
+  ngOnInit() {    
+    this.items = [{
+      label: 'Home',
+      icon: 'pi-home',
+      items: [
+        { label: 'Cadastrar', icon: 'pi pi-pencil', command:(click)=> this.setPage('cad') },
+        { label: 'Listagem', icon: 'pi pi-align-left', command:(click)=> this.setPage('list') }
+      ]
+    }];
+
+  }
+
+  ngOnChanges(){
+    console.log(this.cars)
+  }
+
+  setPage(page){
+    console.log(page)
+  }
+
+  validate(){
+    console.log("!")
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
+import {ButtonModule} from 'primeng/button';
+import {DropdownModule} from 'primeng/dropdown';
+import {CheckboxModule} from 'primeng/checkbox';
+import {CalendarModule} from 'primeng/calendar';
+import {AccordionModule} from 'primeng/accordion';     
+import {InputTextModule} from 'primeng/inputtext';
+import {InputMaskModule} from 'primeng/inputmask';
+import {MenuModule} from 'primeng/menu';
+import {TableModule} from 'primeng/table';
+import { MenuItem } from 'primeng/api'; 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalendarModule,
+    AccordionModule, 
+    FormsModule,
+    ButtonModule,
+    MenuModule,
+    CheckboxModule,
+    InputTextModule,
+    InputMaskModule,
+    TableModule,
+    DropdownModule,
+    BrowserAnimationsModule     
   ],
   providers: [],
   bootstrap: [AppComponent]
